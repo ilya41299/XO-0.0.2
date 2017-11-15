74336c8528a0611c4421953076d51368668a97bd
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-16 00:29:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -79,25 +79,25 @@ int make_move(int move, char op, char *cells) {
 }
 
 char check(char *cells) {
-	for (int i = 0; i < 5; i++) {
-
-		if ((cells[i] == cells[i + 1] && cells[i + 1] == cells[i + 2] && cells[i + 2] == cells[i + 3] && cells[i +3] == cells[i + 4]) && cells[i] != ' ') {
-
+	for (int i = 0; i <=20; i += 5) {
+		if ((cells[i] == cells[i + 1] && cells[i + 1] == cells[i + 2] && cells[i + 2] == cells[i + 3] && cells[i + 3] == cells[i + 4]) && cells[i] != ' ') {
 			return cells[i]; //проверка строк
 		}
-		if ((cells[i] == cells[i + 5] && cells[i + 5] == cells[i + 10] && cells[i + 10] == cells[i + 15] && cells[i + 15] == cells[i + 20]) && cells[i] != ' ') {
-
-			return cells[i]; // проверка столбцов
-		}
-		if ((cells[0] == cells[6] && cells[6] == cells[12] && cells[12] == cells[18] && cells[18] == cells[24]) && cells[i] != ' ') {
-
-			return cells[i]; // проверка главной диагонали
-		}
-		if ((cells[4] == cells[8] && cells[8] == cells[12] && cells[12] == cells[16] && cells[16] == cells[20]) && cells[i] != ' ') {
-
-			return cells[i]; // проверка побочной диагонали
+	}
+	for (int j = 0; j < 4; j++) {
+		if ((cells[j] == cells[j + 5] && cells[j + 5] == cells[j + 10] && cells[j + 10] == cells[j + 15] && cells[j + 15] == cells[j + 20]) && cells[j] != ' ') {
+			return cells[j]; // проверка столбцов
 		}
 	}
+		if ((cells[0] == cells[6] && cells[6] == cells[12] && cells[12] == cells[18] && cells[18] == cells[24]) && cells[0] != ' ') {
+
+			return cells[0]; // проверка главной диагонали
+		}
+		if ((cells[4] == cells[8] && cells[8] == cells[12] && cells[12] == cells[16] && cells[16] == cells[20]) && cells[4] != ' ') {
+
+			return cells[4]; // проверка побочной диагонали
+		}
+	
 
 	return ' ';
 
@@ -121,7 +121,7 @@ int main() {
 		if (k_quit == ex) {
 			return -1;
 		}
-		if (move >= 5) {
+		if (move >= 9) {
 			check(cells);
 			char  win = check(cells);
 			if (win == 'X') {
